90bf66e4b997319a66f4a57234fab9389f703160
Alessio Pareto (MacBook Pro di Pare) <alessio.pareto@nixpare.com>
2023-03-20 17:00:51 +0100

    Source page retreival at runtime

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/nixpare/unimi-dl
 
 go 1.20
+
+require (
+	golang.org/x/net v0.8.0
+	golang.org/x/term v0.6.0
+)
+
+require golang.org/x/sys v0.6.0 // indirect
--- main.go
+++ main.go
@@ -1,200 +1,50 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"os"
-	"strings"
-	"sync"
+
+	"golang.org/x/net/publicsuffix"
 )
 
-type video struct {
-	name string
-	link []string
-}
-
 func main() {
-	dir, _ := os.ReadDir(".")
-	var found bool
+	defer func() {
+		fmt.Println("\nPress Enter to exit")
+		var b []byte
+		fmt.Scanf("%s\n", &b)
+	}()
 
-	pathDir, _ := os.Getwd()
+	logF, _ := os.OpenFile("unimi-dl.log", os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0777)
+	log.SetOutput(logF)
 
-	htmlFiles := make(map[string]string)
-	dirVideos := make(map[string]string)
-
-	for _, x := range dir {
-		if strings.Contains(x.Name(), ".html") {
-			found = true
-			htmlFiles[x.Name()] = pathDir + "/" + x.Name()
-			videoDir := strings.Split(pathDir + "/" + x.Name(), ".")[0]
-			os.MkdirAll(videoDir, 0777)
-			dirVideos[x.Name()] = videoDir
-		}
-	}
-
-	if !found {
-		fmt.Println("Nessun file html presente nella cartella, metterne uno e riavviare il programma\nPremere invio per chiudere la schermata ...")
-		fmt.Scan()
-		return
-	}
-
-	vg := new(sync.WaitGroup)
-	stringErr := new(string)
-
-	for x := range htmlFiles {
-		singleFile(x, dirVideos[x], vg, stringErr)
-	}
-
-	vg.Wait()
-
-	fmt.Println("Il programma ha terminato. Se presenti, gli errori verranno mostrati qui sotto.\nPremere invio per chiudere la schermata ...")
-	fmt.Printf("\nERRORI:\n")
-	if len(*stringErr) == 0 {
-		fmt.Printf("\tNessun Errore\n")
-	} else {
-		fmt.Printf("\n%s\n", *stringErr)
-	}
-	
-	fmt.Scan()
-}
-
-func singleFile(fileName, dirPath string, vg *sync.WaitGroup, stringErr *string) {
-	file, _ := os.Open(fileName)
-	fileScanner := bufio.NewScanner(file)
-	for fileScanner.Scan() {
-		if strings.Contains(fileScanner.Text(), "<title>") {
-			fileScanner.Scan()
-			break
-		}
-	}
-
-	fmt.Printf("Pagina Web trovata: %s\n", fileName)
-
-	videos := make([]video, 0)
-
-	file:for fileScanner.Scan() {
-		if strings.Contains(fileScanner.Text(), "title=\"Lecture Captures\"") {
-			parsedLine := strings.Split(fileScanner.Text(), "<span>")
-			var videoTitle string
-			if index := strings.Index(parsedLine[len(parsedLine)-1], "</span>"); index != -1 {
-				videoTitle = parsedLine[len(parsedLine)-1][:index]
-			}
-			videoLinks := make([]string, 0)
-			for fileScanner.Scan() {
-				if strings.Contains(fileScanner.Text(), "<video") {
-					fileScanner.Scan()
-					parsedLine = strings.Split(fileScanner.Text(), "\"")
-					for _, x := range parsedLine {
-						if strings.Contains(x, "https") {
-							videoLinks = append(videoLinks, x)
-							break
-						}
-					}
-				}
-				if strings.Contains(fileScanner.Text(), "</div></td>") {
-					videos = append(videos, video{
-						videoTitle, videoLinks,
-					})
-					continue file
-				}
-			}
-		}
-	}
-	
-	fmt.Println("Trovati questi video:")
-
-	for _, x := range videos {
-		fmt.Println("Titolo: ", x.name)
-		fmt.Println()
-		for _, s := range x.link {
-			fmt.Println("\tLink: ", s)
-		}
-		fmt.Print("\nVuoi Scaricarli? [S (sì) / n (no)] -> ")
-
-		var resp string
-		fmt.Scanln(&resp)
-		fmt.Print("\n\n")
-
-		if resp == "" || strings.ToLower(resp) == "si" || strings.ToLower(resp) == "sì" || strings.ToLower(resp) == "s" {
-			if len(x.link) > 1 {
-				for i := range x.link {
-					vg.Add(1)
-					go singleVideo(x.link[i], x.name + fmt.Sprintf("_%d", i+1), dirPath, vg, stringErr)
-				}
-			}
-			if len(x.link) == 1 {
-				vg.Add(1)
-				go singleVideo(x.link[0], x.name, dirPath, vg, stringErr)
-			}			
-		}
-	}
-}
-
-func singleVideo(link, name, dirPath string, vg *sync.WaitGroup, stringErr *string) {
-	defer vg.Done()
-	failCount := 0
-
-	resp, err := http.Get(link)
-	for err != nil {
-		failCount ++
-		if failCount > 10 {
-			*stringErr += fmt.Sprintf("Errore dal video %s con link %s: %s\n", name, link, err.Error())
-			return
-		}
-		resp, err = http.Get(link)
-	}
-
-	failCount = 0
-
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil{
-		*stringErr += fmt.Sprintf("Errore video \"%s\" con link \"%s\": %v\n", name, link, err.Error())
-		return
-	}
-
-	parsedVideo := strings.SplitAfter(string(respBody), "chunklist_")
-	if len(parsedVideo) < 2 {
-		*stringErr += fmt.Sprintf("Errore video \"%s\" con link \"%s\": %v\n", name, link, "impossibile trovare il video")
-		return
-	}
-
-	parsedUrl := strings.Split(link, "manifest.m3u8")
-	if len(parsedUrl) != 2 {
-		*stringErr += fmt.Sprintf("Errore video \"%s\" con link \"%s\": %v\n", name, link, "impossibile fare il parsing dell'url")
-		return
-	}
-
-	videoUrl := parsedUrl[0] + parsedVideo[1][:strings.Index(parsedVideo[1], ".")]
-	var index int
-	videoFile, err := os.OpenFile(dirPath + "/" + strings.Replace(name, "/", "-", -1)+".ts", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	client := new(http.Client)
+	cookieJar, err := cookiejar.New(&cookiejar.Options {
+		PublicSuffixList: publicsuffix.List,
+	})
 	if err != nil {
-		*stringErr += fmt.Sprintf("Errore video \"%s\" con link \"%s\": %v\n", name, link, err.Error())
-		return
+		fmt.Printf("Error while initializing the HTTP client")
+		log.Printf("HTTP client error: %v\n", err)
+		os.Exit(1)
 	}
-	defer videoFile.Close()
+	client.Jar = cookieJar
 
-	for {
-		resp, err = http.Get(videoUrl + fmt.Sprintf("_%d.ts", index))
-		for err != nil {
-			failCount ++
-			if failCount > 10 {
-				*stringErr += fmt.Sprintf("Errore video \"%s\" con link \"%s\": %v\n", name, link, "impossibile scaricare il video")
-				return
-			}
-			resp, err = http.Get(videoUrl + fmt.Sprintf("_%d.ts", index))
-		}
-		respBody, err = io.ReadAll(resp.Body)
-		if err != nil{
-			*stringErr += fmt.Sprintf("Errore video \"%s\" con link \"%s\": %v\n", name, link, err.Error())
-			return
-		}
-		if len(respBody) == 0 {
-			break
-		}
-		videoFile.Write(respBody)
-
-		index ++
+	err = performLogin(client)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+
+	page, err := getPage(
+		client,
+		"https://nbasilicoae2.ariel.ctu.unimi.it/v5/frm3/ThreadList.aspx?fc=qBg4sBrRnwcdhrbedslZntFd2HdJGwehSpagKzRGGL46du5ML7nAZ1F3iVRHQ0jk&roomid=227362",
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(page)
 }
