4fb05403cb0eeedf659ddc63db419778611daacb
Alessio Pareto (Desktop-Pare) <76201865+alessio-pareto@users.noreply.github.com>
2023-04-08 19:17:50 +0200

    Added documentation and changed http request library

--- no_ffmpeg.py
+++ no_ffmpeg.py
@@ -1,26 +1,61 @@
+from dataclasses import dataclass
+
 import io
 import os
 import queue
+import requests
 import threading
-import urllib.request as req
 
+import urllib3
+urllib3.disable_warnings()
+
+@dataclass
 class VideoSegment:
+    """VideoSegment contains the data of the video segment downloaded
+    from the video manifest and a flag that will tell when the data is
+    ready to be written to the file."""
+
     data: bytes
+    """The data of the segment"""
+
     ready: bool
+    """True if the segment is ready to be written"""
 
     def __init__(self):
         self.data = None
         self.ready = False
 
+@dataclass
 class Video:
+    """Video is the class that handles the multithreaded download of its
+    segments and the creation of the file.
+
+    The download process is regulated by the constants __concurrent_downloads__
+    and __flush_after__: the first one determines how many videos can be downloaded
+    simultaneously, the second one determines how ofter to write the segments to the
+    file. However if the __flush_after__ is less that __concurrent_downloads__, the
+    latter will be used"""
+
     name: str
+    """The video name to be used for the file name"""
+
     url: str
+    """The url of the video manifest of type .m3u8"""
+
     segments: dict[int, VideoSegment]
+    
     file: io.BufferedWriter
+
     download_queue: queue.Queue
+    """Queue on which every background thread handling the segment download report
+    which segment (by sending its index) is ready to be written. Currently the index
+    reported is not used, but clould be for optimization"""
 
     __concurrent_downloads__ = 8
-    __flush_after__ = 4
+    """The number of concurrent background threads used to download video segments"""
+
+    __flush_after__ = 20
+    """The minimum number of subsequent segments needed before flushing them to the file"""
 
     def __init__(self, name: str, url: str):
         self.name = name
@@ -31,10 +66,12 @@ class Video:
         video_path = os.getcwd() + "/" + self.name + ".ts"
         self.file = open(video_path, 'wb')
 
-    def download_segment(self, index: int, url: str):
-        response = req.urlopen(url)
+    def download_segment(self, index: int, seg_url: str):
+        
+        response = requests.get(seg_url, verify=False)
+        print(response.status_code)
 
-        self.segments[index].data = response.read()
+        self.segments[index].data = response.content
         self.segments[index].ready = True
 
         self.download_queue.put(item=index, block=False)
@@ -43,6 +80,8 @@ class Video:
         slice_start = -1
 
         for i in range(len(self.segments)):
+            """Searches the first item that is ready (download completed) and has the data
+            field not None (otherwise this means that the segment was already written to the file)"""
             if (self.segments[i].ready and self.segments[i].data != None):
                 slice_start = i
                 break
@@ -51,29 +90,34 @@ class Video:
             return
         
         slice_end = slice_start
+        """Searches how many consecutive segments there are with the same criteria above"""
         for i in range(slice_start+1, len(self.segments)):
             if (self.segments[i].ready == False):
                 break
             slice_end = i
 
+        """If there are not enough consecutive segments to be written, it does nothing.
+        Flushing too few segments can lead to worse performance and to errors in the
+        correct write order (this is unclear why, but if flushing with a rate minor
+        to the number of concurrent downloads, the file will be corrupted)"""
         flush_after = max(self.__flush_after__, self.__concurrent_downloads__)
         if (slice_end - slice_start < flush_after - 1):
             return
-
-        print(f'Flushing segments from {slice_start} to {slice_end}')
         
-        for i in range(slice_start, slice_end+1):
+        for i in range(slice_start, slice_end + 1):
             self.file.write(self.segments[i].data)
+            """Setting the segment data to None both as a flat to tell that the
+            segment was written and to free up the memory"""
             self.segments[i].data = None
 
     def download_video(self):
-        print(f"Downloading video {self.name} with url {self.url}")
+        print(f"Downloading video <{self.name}> with url <{self.url}>")
         
-        manifest_content = str(req.urlopen(self.url).read(), encoding='utf-8')
+        manifest_content = requests.get(self.url, verify=False).text
         video_base_url = self.url[:self.url.rindex("/")]
 
         segment_manifest_url = video_base_url + "/" + manifest_content.rstrip(" \n").split("\n")[-1]
-        segment_manifest_content = str(req.urlopen(segment_manifest_url).read(), encoding='utf-8')
+        segment_manifest_content = requests.get(segment_manifest_url, verify=False).text
 
         segment_urls = segment_manifest_content.split("\n")
         for i in range(len(segment_urls)):
@@ -90,13 +134,15 @@ class Video:
             seg_counter += 1
 
         next_downloading = 0
-
         while (next_downloading < seg_counter):
+            """Fills the concurrent background threads with the first
+            <__concurrent_downloads__> segments"""
             if (next_downloading < self.__concurrent_downloads__):
                 seg_threads[next_downloading].start()
                 next_downloading += 1
                 continue
 
+            """Waits for one of the segments in download to complete"""
             self.download_queue.get()
 
             seg_threads[next_downloading].start()
@@ -109,7 +155,7 @@ class Video:
 
         self.flush()
 
-        print(f"Video {self.name} downloaded")
+        print(f"Video <{self.name}> downloaded")
 
 if (__name__ == "__main__"):
     v = Video(name="test/Video_1", url="https://videolectures.unimi.it/vod/mp4:F1X77-23-2023-02-27%2011-06-16.mp4/manifest.m3u8")
