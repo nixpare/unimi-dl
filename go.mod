0c25326b88b8199019cd35322e3ae3fb2a177056
Alessio Pareto (Desktop-Pare) <76201865+alessio-pareto@users.noreply.github.com>
2023-04-14 12:22:13 +0200

    Added keyboard listener

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/eiannone/keyboard v0.0.0-20220611211555-0d226195f203
 	golang.org/x/net v0.8.0
 	golang.org/x/term v0.6.0
 )
--- main.go
+++ main.go
@@ -4,15 +4,29 @@ import (
 	"fmt"
 	"log"
 	"os"
+
+	"github.com/eiannone/keyboard"
 )
 
 const TEST_PAGE = "https://nbasilicoae2.ariel.ctu.unimi.it/v5/frm3/ThreadList.aspx?fc=BxPlwgRH%2b296WJzdnwYOWDcStR%2fDkD%2fqUMKoIPKGaa2N408CbbDWmWxRudLIAoTV&roomid=227362"
 
 func main() {
 	defer func() {
+		keyEvents, err := keyboard.GetKeys(50)
+		if err != nil {
+			return
+		}
+
 		fmt.Println("\nPress Enter to exit")
-		var b []byte
-		fmt.Scanf("%s\n", &b)
+		for e := range keyEvents {
+			if e.Err != nil {
+				continue
+			}
+
+			if e.Key == keyboard.KeyEnter {
+				break
+			}
+		}
 	}()
 
 	logF, _ := os.OpenFile("unimi-dl.log", os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0777)
